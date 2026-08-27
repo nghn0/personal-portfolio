a043275ace7c20b573cf909fcb5842a965c46d46
nghn0 <nithi9905@gmail.com>
2026-08-27 22:50:44 +0530

    feat: Update experience cards to horizontal how-it-works layout

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -175,10 +175,10 @@ export default function Experience() {
               }}
               className={
                 index === 0
-                  ? "relative md:absolute md:top-0 md:left-[2%] mb-10 md:mb-0"
+                  ? "relative md:absolute md:top-0 md:left-0 mb-10 md:mb-0 start-0"
                   : index === 1
-                  ? "relative md:absolute md:top-[80px] md:right-[2%]"
-                  : "relative md:absolute md:top-[260px] md:left-[2%]"
+                  ? "relative md:absolute md:top-0 md:left-[140px] md:start-0"
+                  : "relative md:absolute md:top-0 md:left-[280px] md:start-0"
               }
             >
               <div
